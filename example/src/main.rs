10ddf81b7b9364239eced4d600e4536c33ed284e
Vlad Frolov <frolvlad@gmail.com>
2021-11-21 17:49:08 +0200

    updated example with StatusBox abstraction

--- example/src/main.rs
+++ example/src/main.rs
@@ -3,15 +3,36 @@ use inquire::Text;
 type AccountId = String;
 type Message = String;
 
+struct StatusBox {
+    storage: std::collections::HashMap<AccountId, Message>,
+}
+
+impl StatusBox {
+    pub fn new() -> Self {
+        Self {
+            //storage: std::collections::HashMap<AccountId, Message>::new(),
+            storage: Default::default(),
+        }
+    }
+
+    pub fn set_message(&mut self, username: AccountId, message: Message) {
+        self.storage.insert(username, message);
+    }
+
+    pub fn get_message(&self, username: AccountId) -> Option<&Message> {
+        self.storage.get(&username)
+    }
+}
+
 fn main() {
-    let mut storage = std::collections::HashMap::<AccountId, Message>::new();
+    let mut status_box = StatusBox::new();
 
     loop {
         let command = Text::new("Какую команду выполнить?").prompt().unwrap();
         match command.as_str() {
             "get" => {
                 let username = Text::new("Какой пользователь?").prompt().unwrap();
-                if let Some(message) = storage.get(&username) {
+                if let Some(message) = status_box.get_message(username.clone()) {
                     println!("Status message: {}", message);
                 } else {
                     println!("There is no status message set for the user '{}' yet", username);
@@ -20,7 +41,7 @@ fn main() {
             "set" => {
                 let username = Text::new("Какой пользователь?").prompt().unwrap();
                 let message = Text::new("Какой соощение статуса?").prompt().unwrap();
-                storage.insert(username, message);
+                status_box.set_message(username, message);
             }
             _ => {
                 println!("такой команды нет");
