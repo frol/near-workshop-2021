155e6407f64459d90f634db78c0d766b4ae48478
Vlad Frolov <frolvlad@gmail.com>
2021-11-21 23:05:23 +0200

    migrated to the near_sdk::collection::LookupMap from std::collections::HashMap

--- status-box/src/lib.rs
+++ status-box/src/lib.rs
@@ -1,15 +1,14 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::near_bindgen;
-use near_sdk::serde::Serialize;
 
 type AccountId = String;
 type Message = String;
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize, Serialize, Default)]
-#[serde(crate = "near_sdk::serde")]
+#[derive(BorshDeserialize, BorshSerialize, near_sdk::PanicOnDefault)]
 pub struct StatusBox {
-    storage: std::collections::HashMap<AccountId, Message>,
+    // Read more: https://www.near-sdk.io/contract-structure/collections
+    storage: near_sdk::collections::LookupMap<AccountId, Message>,
 }
 
 #[near_bindgen]
@@ -17,15 +16,15 @@ impl StatusBox {
     #[init]
     pub fn new() -> Self {
         Self {
-            storage: Default::default(),
+            storage: near_sdk::collections::LookupMap::new(b"s"),
         }
     }
 
     pub fn set_message(&mut self, username: AccountId, message: Message) {
-        self.storage.insert(username, message);
+        self.storage.insert(&username, &message);
     }
 
     pub fn get_message(&self, username: AccountId) -> Option<Message> {
-        self.storage.get(&username).cloned()
+        self.storage.get(&username)
     }
 }
