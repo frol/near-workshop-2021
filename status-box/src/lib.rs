5b022b3cbf4468f445d6c3b9cafbf76bad1f3134
Vlad Frolov <frolvlad@gmail.com>
2021-11-21 17:56:46 +0200

    initial status-box implementation

--- status-box/src/lib.rs
+++ status-box/src/lib.rs
@@ -1,7 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::near_bindgen;
+
+type AccountId = String;
+type Message = String;
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, Default)]
+pub struct StatusBox {
+    storage: std::collections::HashMap<AccountId, Message>,
+}
+
+#[near_bindgen]
+impl StatusBox {
+    pub fn new() -> Self {
+        Self {
+            storage: Default::default(),
+        }
+    }
+
+    pub fn set_message(&mut self, username: AccountId, message: Message) {
+        self.storage.insert(username, message);
+    }
+
+    pub fn get_message(&self, username: AccountId) -> Option<Message> {
+        self.storage.get(&username).cloned()
     }
 }
