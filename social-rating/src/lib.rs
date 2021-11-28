0c4e1c6eeb82ce5cdd8258d3e6be3e2bcb76b29c
Vlad Frolov <frolvlad@gmail.com>
2021-11-28 17:11:07 +0200

    tmp

--- social-rating/src/lib.rs
+++ social-rating/src/lib.rs
@@ -6,16 +6,39 @@ impl App {
     }
 
     pub fn add_user(&mut self, account_id: near_sdk::AccountId) {
-        near_sdk::env::set()
+        near_sdk::env::storage_write(b"qq", b"hello");
+    }
+
+    pub fn get_user(&mut self, account_id: near_sdk::AccountId) -> Option<Vec<u8>> {
+        near_sdk::env::storage_read(b"qq")
     }
 }
 
-#[cfg(test)]
+#[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
+    use std::convert::TryInto;
+    use std::str::FromStr;
+
+    use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::{testing_env, VMContext};
+    
     use super::*;
 
+    fn get_context(is_view: bool) -> VMContext {
+        VMContextBuilder::new()
+            .signer_account_id(near_sdk::AccountId::from_str("bob_near").unwrap())
+            .is_view(is_view)
+            .build()
+    }
+
     #[test]
-    fn it_works() {
-        let app = App::new();
+    fn my_test() {
+        let context = get_context(false);
+        testing_env!(context);
+        
+        let mut app = App::new();
+
+        app.add_user(near_sdk::AccountId::from_str("asd").unwrap());
+        assert!(app.get_user(near_sdk::AccountId::from_str("asd").unwrap()).is_none());
     }
 }
